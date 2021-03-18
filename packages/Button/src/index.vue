362ab04c205c3d24b5a4c35ab075bb453e72f76b
ZMT <ZMT819@126.com>
2021-03-18 17:04:25 +0800

    fix button color

--- packages/Button/src/index.vue
+++ packages/Button/src/index.vue
@@ -1,27 +1,58 @@
 <template>
-  <button @click="onClick">
+  <button class="z-button" :class="['z-button--'+ type, 'z-button--'+ size]">
     <slot></slot>
   </button>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-import VueTypes from 'vue-types'
 export default defineComponent({
   name: 'z-button',
 
   props: {
-    type: VueTypes.string.def('primary')
+    type: {
+      type: String,
+      default: 'default',
+      validator: (value: string) => {
+        return ['success', 'danger', 'info', 'primary', 'warning', 'default'].indexOf(value) > -1
+      }
+    },
+
+    size: {
+      type: String,
+      default: 'normal',
+      validator: (value: string) => {
+        return ['mini', 'small', 'large', 'normal'].indexOf(value) > -1
+      }
+    }
   },
 
-  setup (props, { emit }) {
-    function onClick () {
-      emit('click')
-    }
-
-    return { onClick }
+  setup () {
+    return {}
   }
 })
 </script>
 <style lang="scss" scoped>
-
+@import '../../styles/handler';
+.z-button{
+  border: 0;
+  outline: 0;
+  text-align: center;
+  color: getColor('default');
+  &--primary{
+    background-color: getColor('primary');
+  }
+  &--success{
+    background-color: getColor('success');
+  }
+  &--info{
+    background-color: getColor('info');
+  }
+  &--warning{
+    background-color: getColor('warning');
+  }
+  &--default{
+    background-color: getColor('default');
+    color: #000
+  }
+}
 </style>
