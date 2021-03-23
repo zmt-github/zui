8c047a25c153a847b0887be72d3af8111c3e5494
ZMT <ZMT819@126.com>
2021-03-23 17:19:40 +0800

    feat chekbox

--- packages/index.ts
+++ packages/index.ts
@@ -2,12 +2,14 @@ import { App, DefineComponent } from 'vue'
 import ZButton from './Button'
 import ZLink from './Link'
 import ZRadio from './Radio'
+import ZCheckbox from './Checkbox'
 
 // eslint-disable-next-line
 const components: Array<any> = [
   ZButton,
   ZLink,
-  ZRadio
+  ZRadio,
+  ZCheckbox
 ]
 
 function install (app: App): void {
@@ -18,5 +20,6 @@ export default {
   install,
   ZButton,
   ZLink,
-  ZRadio
+  ZRadio,
+  ZCheckbox
 }
