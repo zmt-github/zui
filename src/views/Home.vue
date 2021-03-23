673c833b7830dd10b4d2438c8d321cd4473ebf56
ZMT <ZMT819@126.com>
2021-03-23 17:19:48 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <z-radio v-model="value" size='mini' border  label='1' name='sex' type='success'>男</z-radio>
     <z-radio v-model="value" border label='2' name='sex' type='success'>女</z-radio>
+    <z-checkbox v-model="checked" >女</z-checkbox>
   </div>
 </template>
 
@@ -13,12 +14,13 @@ export default defineComponent({
 
   setup () {
     const value = ref('1')
+    const checked = ref(false)
 
     function change () {
       console.log(value.value)
     }
 
-    return { value, change }
+    return { value, change, checked }
   }
 })
 </script>
